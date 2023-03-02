b26c772d929450026854f6ed252f4d9e54cec6ce
GonzaloCozP <126581733+GonzaloCozP@users.noreply.github.com>
2023-03-02 10:23:19 -0500

    Add files via upload

--- promedio.cpp
+++ promedio.cpp
@@ -1,16 +1,40 @@
 #include <iostream>
 using namespace std;
-double prom (double n1, double n2, double n3);
-double prom (double n1, double n2, double n3){
+int prom (int n1, int n2, int n3);
+int prom (int n1, int n2, int n3){
     double prom=0;
     prom=(n1+n2+n3)/3;
     return prom;
 }
+int notas(int calcula){
+    int nota=0;
+    if (calcula>=18){
+            nota=1;
+    }else{
+        if (calcula>15){
+            nota=2;
+    }else{
+        if (calcula>11){
+            nota=3;
+
+    }else{
+        if (calcula>5){
+            nota=4;
+
+    }else{
+        nota=5;
+    }
+    }
+    }
+    }
+return nota;
+}
 int main(){
-    double n1;
-    double n2;
-    double n3;
-    double res;
+    int nota;
+    int n1;
+    int n2;
+    int n3;
+    int res;
     cout<<"Promedio de notas"<<endl;
     cout<<"Inserte nota 1"<<endl;
     cin>>n1;
@@ -20,13 +44,26 @@ int main(){
     cin>>n3;
     cout<<"La Respuesta es:"<<endl;
     res = prom (n1,n2,n3);
-    
-    if(res<=10.5){
-        cout<< res<<endl <<"Desaprobaste"<<endl;
-        } else {
-            cout<< res<<endl <<"Aprobaste"<<endl;
-
-        }
-
+    nota=notas(res);
+    switch(nota){
+        case 1: 
+        cout<<"A";
+        break;
+        case 2: 
+        cout<<"B";
+        break;
+        case 3: 
+        cout<<"C";
+        break;
+        case 4: 
+        cout<<"F";
+        break;
+        case 5: 
+        cout<<"G";
+        break;
+        default:
+        cout<<"Valor invorrecto";
+        break;
+    }
     return 0;
 }
